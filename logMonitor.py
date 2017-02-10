4c32d2128a4498db266aec97003d2ac5dcf026d4
winterinferno <dabeckham@ucdavis.edu>
2017-02-10 03:02:45 -0800

    Added the capability to write log path and contents to file and remove exisiting log files if they already exist

--- logMonitor.py
+++ logMonitor.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 import subprocess
+import os
 
 def connectDB():
     return sqlite3.connect('logs.db')
@@ -36,16 +37,34 @@ def printDB():
     for row in c.execute('SELECT * FROM logs'):
         print(row)
 
-logs = open('/home/darrylb/logMonitor/logs.txt', 'w')
+log_paths = "/home/darrylb/logMonitor/log_paths.txt"
+log_contents = "/home/darrylb/logMonitor/log_contents.txt"
+
+if os.path.isfile(log_paths):
+    os.remove(log_paths)
+if os.path.isfile(log_contents):
+    os.remove(log_contents)
+
+#opens file for writing
+logs = open('/home/darrylb/logMonitor/log_paths.txt', 'w')
+
+#calls find searching from / looking for files that end in .log, ignoring directories in my home which don't begin with '.'
 subprocess.call(['find','/','-name','*\.log','-not','-path','/home/darrylb/[A-Za-z0-9]*'], stdout=logs)
 logs.close()
 
-with open('/home/darrylb/logMonitor/logs.txt', 'r') as f:
+#opens file containing log paths
+with open(log_paths, 'r+') as f:
+    #reads path ignoring any newline characters
     paths = f.read().splitlines()
-    for path in paths:
-        print(path)
-        subprocess.call(['grep','-i','-E','error', '%s' % path])
-        print()
+    with open(log_contents,'w+') as g:
+    #for every log file
+        for path in paths:
+            subprocess.call(['printf', path+'\n'], stdout=g)
+            #search for terms that indicate problems
+            subprocess.call(['grep','-i','-E','error|fail|unable|fatal|broken', '%s' % path], stdout=g)
+            subprocess.call(['printf',"'\n\n'"], stdout=g)
+        g.close()
+    f.close()
 
 '''
 createTable()
