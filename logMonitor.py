cc2aa24073326f590d211f0eb124448850607766
winterinferno <dabeckham@ucdavis.edu>
2017-02-11 19:13:27 -0800

    Added web interface. Output still goes to terminal. Will format data for web browser. Also still need to count number of occurances of error in log

--- logMonitor.py
+++ logMonitor.py
@@ -4,14 +4,16 @@
 import sqlite3
 import subprocess
 import os
+from flask import Flask, render_template
 
-log_db_path = "/home/darrylb/darrylbckhm/logMonitor/log.db"
+log_db_path = "/home/darrylb/darrylbckhm/logMonitor/logs.db"
+log_paths = "/home/darrylb/darrylbckhm/logMonitor/log_paths.txt"
+log_contents = "/home/darrylb/darrylbckhm/logMonitor/log_contents.txt"
+
+app = Flask(__name__)
 
 def connect_db():
-    return sqlite3.connect('logs.db')
-
-conn = connect_db()
-c = conn.cursor()
+    return sqlite3.connect('/home/darrylb/darrylbckhm/logMonitor/logs.db')
 
 def save_db():
     conn.commit()
@@ -48,9 +50,6 @@ def delete_db():
     if os.path.isfile(log_db_path):
         os.remove(log_db_path)
 
-log_paths = "/home/darrylb/darrylbckhm/logMonitor/log_paths.txt"
-log_contents = "/home/darrylb/darrylbckhm/logMonitor/log_contents.txt"
-create_table()
 
 def log_clean():
     if os.path.isfile(log_paths):
@@ -61,10 +60,9 @@ def log_clean():
 def get_paths():
     #opens file for writing
     logs = open('/home/darrylb/darrylbckhm/logMonitor/log_paths.txt', 'w')
-
     #calls find searching from / looking for files that end in .log, ignoring directories in my home which don't begin with '.'
     #changed to account for auto mounted filesystems in /run/media, as well as a wildcard added to match ALL home directories
-    subprocess.call(['find','/','-name','*\.log','-not','-path','/home/*/[A-Za-z0-9]*', '-not', '-path', '/run/media/*'], stdout=logs)
+    subprocess.call(['find','/', '-type', 'f', '-name','*[^a-zA-Z0-9]log','-not','-path','/home/*/[A-Za-z0-9]*', '-not', '-path', '/run/media/*', '-not','-path','/proc/*','-not','-path','*/bin/*', '-not', '-path', '*/sbin/*'], stdout=logs)
     logs.close()
 
 def get_log_contents():
@@ -82,24 +80,33 @@ def get_log_contents():
                 subprocess.call(['grep','-i','-E','error|fail|unable|fatal|broken', '%s' % path], stdout=g)
                 subprocess.call(['printf','\n'], stdout=g)
                 #FIX ME: The idea is to use a dictionary with the path as the key and the contents of the log file in a list of entries corresponding to the number of lines in the log file
-                log_map[path]=open(path,'r').readlines()
-                log_map[path]=[line.strip() for line in log_map[path]]
-                insert_db(log_map)
+                if os.path.isfile(path):
+                    try:
+                        log_map[path]=open(path,'r').readlines()
+                        log_map[path]=[line.strip() for line in log_map[path]]
+                        insert_db(log_map)
+                    except:
+                        print("Failed to read bytes from:", path)
                 #print(log_map[path])
                 #print()
             g.close()
         f.close()
 
-log_clean()
-get_paths()
-get_log_contents()
-print_db()
 delete_db()
+conn = connect_db()
+c = conn.cursor()
+create_table()
+log_clean()
 
-'''
-createTable()
-insertDB()
-saveDB()
-clearDB()
-closeDB()
-'''
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/logs', methods=['GET'])
+def logs():
+    get_paths()
+    get_log_contents()
+    print_db()
+
+if __name__ == "__main__":
+    app.run()
